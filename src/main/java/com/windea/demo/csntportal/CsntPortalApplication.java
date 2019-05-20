f95f13d67a62fe5e0414ffb070aa2ca39da55013
微风的龙骑士 风游迩 <2944262241@qq.com>
2019-05-20 13:14:09 +0800

    居中container

--- src/main/java/com/windea/demo/csntportal/CsntPortalApplication.java
+++ src/main/java/com/windea/demo/csntportal/CsntPortalApplication.java
@@ -1,9 +1,9 @@
 package com.windea.demo.csntportal;
 
-import com.windea.demo.csntportal.interceptor.LogInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * SpringBoot应用程序的启动类。
@@ -18,11 +18,6 @@ public class CsntPortalApplication implements WebMvcConfigurer {
 			.maxAge(3600);
 	}
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		//配置拦截器
-		registry.addInterceptor(new LogInterceptor()).addPathPatterns("/**");
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsntPortalApplication.class, args);
