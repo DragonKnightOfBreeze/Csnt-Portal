1b4153d09ebdfac92b098f891ffc9fe3078264ca
微风的龙骑士 风游迩 <2944262241@qq.com>
2019-06-14 17:07:15 +0800

    1.2.8 完成各种column-detail组件的html、ts（复制粘贴）。

--- src/main/mobile/src/app/page/tabs/info/reform-column-detail/reform-column-detail.page.ts
+++ src/main/mobile/src/app/page/tabs/info/reform-column-detail/reform-column-detail.page.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from "@angular/core";
+import {ReformColumn} from "../../../../domain/entity/ReformColumn";
+import {UserService} from "../../../../service/api/user.service";
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+import {ReformColumnService} from "../../../../service/api/reform-column.service";
 
 @Component({
   selector: "app-reform-column-detail",
@@ -6,11 +11,36 @@ import {Component, OnInit} from "@angular/core";
   styleUrls: ["./reform-column-detail.page.scss"],
 })
 export class ReformColumnDetailPage implements OnInit {
+  column: ReformColumn;
 
-  constructor() {
+  id: number;
+
+  constructor(private service: ReformColumnService,
+              private userService: UserService,
+              private route: ActivatedRoute,
+              private location: Location) {
   }
+
 
   ngOnInit() {
+    this.id = +this.route.snapshot.paramMap.get("id");
+    this.get();
   }
 
+  private get() {
+    this.service.get(this.id).subscribe(column => {
+      this.column = column;
+    });
+  }
+
+  delete() {
+    this.location.back();
+    this.service.delete(this.column.id).subscribe();
+  }
+
+  update() {
+    this.service.update(this.column).subscribe(updatedColumn => {
+      this.column = updatedColumn;
+    });
+  }
 }
--- src/main/mobile/src/app/page/tabs/info/study-column-detail/study-column-detail.page.ts
+++ src/main/mobile/src/app/page/tabs/info/study-column-detail/study-column-detail.page.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from "@angular/core";
+import {UserService} from "../../../../service/api/user.service";
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+import {StudyColumn} from "../../../../domain/entity/StudyColumn";
+import {StudyColumnService} from "../../../../service/api/study-reform.service";
 
 @Component({
   selector: "app-study-column-detail",
@@ -6,11 +11,36 @@ import {Component, OnInit} from "@angular/core";
   styleUrls: ["./study-column-detail.page.scss"],
 })
 export class StudyColumnDetailPage implements OnInit {
+  column: StudyColumn;
 
-  constructor() {
+  id: number;
+
+  constructor(private service: StudyColumnService,
+              private userService: UserService,
+              private route: ActivatedRoute,
+              private location: Location) {
   }
+
 
   ngOnInit() {
+    this.id = +this.route.snapshot.paramMap.get("id");
+    this.get();
   }
 
+  private get() {
+    this.service.get(this.id).subscribe(column => {
+      this.column = column;
+    });
+  }
+
+  delete() {
+    this.location.back();
+    this.service.delete(this.column.id).subscribe();
+  }
+
+  update() {
+    this.service.update(this.column).subscribe(updatedColumn => {
+      this.column = updatedColumn;
+    });
+  }
 }
